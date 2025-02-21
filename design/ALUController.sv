bdaab1cc4a2ffd95de857ac3d7886b87f45418dc
victoriapessoabm <vpbm@cin.ufpe.br>
2025-02-20 21:54:59 -0300

    instrucoes implementadas em alucontroller

--- design/ALUController.sv
+++ design/ALUController.sv
@@ -1,6 +1,9 @@
 `timescale 1ns / 1ps
 
 /*
+A ALUController está dentro do processador, na Unidade de Controle. Ela recebe informações derivadas das instruções que estão sendo lidas (ALUOp, Funct3, Funct7) e geram um código Operation.
+O código Operation que define qual é a operação que será realizada dentro da ALU.
+
 A unidade de controle (ALUController) vê o Opcode(7 bits) e define qual é a classe das instruções que a ALU fará (ALUOp)
 O ALUController usa então o Funct3 e o Funct7 para refinarem qual é a operação específica dentro da ALU
 Operation é então o código final que a ALU usa para realizar a operação
@@ -14,11 +17,13 @@ module ALUController (
     input logic [2:0] Funct3,  // I - usado para diferenciar operações dentro de uma mesma classe de instruções
 
     // Output
-    output logic [3:0] Operation  // operação selecionada para a ALU
+    output logic [3:0] Operation  // operação selecionada para a ALU (saída)
 );
 
   assign Operation[0] = 
                         ((ALUOp == 2'b10) && (Funct3 == 3'b110)) ||                            // OR, ORI
+                        ((ALUOp == 2'b10) && (Funct3 == 3'b000 && Funct7 == 7'b0100000))       // SUB
+                        ((ALUOp == 2'b10) && (Funct3 == 3'b010) && (Funct7 == 7'b0000000))     // SLT
                         ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000)) ||  // ADD 
                         ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||  // ADDI
                         ((ALUOp == 2'b01) && (Funct3 == 3'b001)) ||                            // BNE
@@ -27,17 +32,20 @@ module ALUController (
                         ((ALUOp == 2'b01) && (Funct3 == 3'b111));                              // BGEU
 
   assign Operation[1] = 
-                        (ALUOp == 2'b00) ||  // LW\SW
-                        ((ALUOp == 2'b10) && (Funct3 == 3'b000)) ||  // R\I-add
+                        (ALUOp == 2'b00) ||                                                    // LW\SW
+                        ((ALUOp == 2'b10) && (Funct3 == 3'b000)) ||                            // R\I-add
+                        ((ALUOp == 2'b10) && (Funct3 == 3'b110) && (Funct7 == 7'b0000000))     // OR
                         ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||  // R\I->>>
                         ((ALUOp == 2'b01) && (Funct3 == 3'b101)) ||                            // BGE
                         ((ALUOp == 2'b01) && (Funct3 == 3'b110));                              // BLTU
 
   assign Operation[2] =  
-                        ((ALUOp==2'b10) && (Funct3==3'b101) && (Funct7==7'b0000000)) || // R\I->>
+                        ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000)) ||   // R\I->>
+                        ((ALUOp == 2'b10) && (Funct3 == 3'b100) && (Funct7 == 7'b0000000))     // XOR
+                        ((ALUOp == 2'b10) && (Funct3 == 3'b010) && (Funct7 == 7'b0000000))     // SLT
                         ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||  // R\I->>>
-                        ((ALUOp == 2'b10) && (Funct3 == 3'b001)) ||  // R\I-<<
-                        ((ALUOp == 2'b10) && (Funct3 == 3'b010)) ||  // R\I-<
+                        ((ALUOp == 2'b10) && (Funct3 == 3'b001)) ||                            // R\I-<<
+                        ((ALUOp == 2'b10) && (Funct3 == 3'b010)) ||                            // R\I-<
                         ((ALUOp == 2'b11) && (Funct3 == 3'b000)) ||                            // JALR
                         ((ALUOp == 2'b01) && (Funct3 == 3'b110)) ||                            // BLTU
                         ((ALUOp == 2'b01) && (Funct3 == 3'b111));                              // BGEU
